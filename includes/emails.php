7a847feae2de6ac4db312e2d477d93842673d7e9
Spencer Finnell <spencerfinnell@gmail.com>
2013-03-04 14:53:39 -0500

    Start emails. See #34

---
+++ includes/emails.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Trigger Purchase Receipt
+ *
+ * Causes the purchase receipt to be emailed when initially pledged.
+ */
+function atcf_trigger_pending_purchase_receipt( $payment_id, $new_status, $old_status ) {
+	// Make sure we don't send a purchase receipt while editing a payment
+	if ( isset( $_POST['edd-action'] ) && $_POST['edd-action'] == 'edit_payment' )
+		return;
+
+	// Check if the payment was already set to complete
+	if ( $old_status == 'publish' || $old_status == 'complete' )
+		return; // Make sure that payments are only completed once
+
+	// Make sure the receipt is only sent when new status is preapproval
+	if ( $new_status != 'preapproval' )
+		return;
+
+	// Send email with secure download link
+	atcf_email_pending_purchase_receipt( $payment_id );
+}
+add_action( 'edd_update_payment_status', 'edd_trigger_purchase_receipt', 10, 3 );
+
+function atcf_email_pending_purchase_receipt( $payment_id, $admin_notice = true ) {
+	global $edd_options;
+
+	$payment_data = edd_get_payment_meta( $payment_id );
+	$user_info = maybe_unserialize( $payment_data['user_info'] );
+
+	if ( isset( $user_info['id'] ) && $user_info['id'] > 0 ) {
+		$user_data = get_userdata($user_info['id']);
+		$name = $user_data->display_name;
+	} elseif ( isset( $user_info['first_name'] ) && isset( $user_info['last_name'] ) ) {
+		$name = $user_info['first_name'] . ' ' . $user_info['last_name'];
+	} else {
+		$name = $user_info['email'];
+	}
+
+	$message = edd_get_email_body_header();
+	$message .= atcf_get_email_body_content( $payment_id, $payment_data );
+	$message .= edd_get_email_body_footer();
+
+	$from_name = isset( $edd_options['from_name'] ) ? $edd_options['from_name'] : get_bloginfo('name');
+	$from_email = isset( $edd_options['from_email'] ) ? $edd_options['from_email'] : get_option('admin_email');
+
+	$subject = apply_filters( 'atcf_pending_purchase_subject', __( 'Your pledge has been received', 'atcf' ), $payment_id );
+
+	$subject = edd_email_template_tags( $subject, $payment_data, $payment_id );
+
+	$headers = "From: " . stripslashes_deep( html_entity_decode( $from_name, ENT_COMPAT, 'UTF-8' ) ) . " <$from_email>\r\n";
+	$headers .= "Reply-To: ". $from_email . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=utf-8\r\n";
+
+	// Allow add-ons to add file attachments
+	$attachments = apply_filters( 'atcf_pending_receipt_attachments', array(), $payment_id, $payment_data );
+
+	wp_mail( $payment_data['email'], $subject, $message, $headers, $attachments );
+
+	if ( $admin_notice ) {
+		do_action( 'edd_admin_pending_purchase_notice', $payment_id, $payment_data );
+	}
+}
+
+function atcf_get_email_body_content( $payment_id = 0, $payment_data = array() ) {
+	global $edd_options;
+
+	$default_email_body = __( 'Dear {name}', 'atcf' ) . "\n\n";
+	$default_email_body .= sprintf( __( 'Thank you for your pledge. You will only be charged your pledge amount if the %s receives 100% funding.', 'atcf' ), strtolower( edd_get_label_singular() ) ) . "\n\n";
+	$default_email_body .= "{sitename}";
+
+	$email = $default_email_body;
+
+	$email_body = edd_email_template_tags( $email, $payment_data, $payment_id );
+
+	return apply_filters( 'atcf_pending_purchase_receipt', $email_body, $payment_id, $payment_data );
+}
