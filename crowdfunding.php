9e49016eccf97f0e6bf9f049345b60c0c22eaee1
Adam Pickering (Mint Themes <adam@mintthemes.com>
2013-03-15 01:20:13 -0300

    0.4.1
    0.4.1

--- crowdfunding.php
+++ crowdfunding.php
@@ -5,7 +5,7 @@
  * Description: A crowd funding platform in the likes of Kickstarter and Indigogo
  * Author:      AppThemer
  * Author URI:  http://appthemer.com
- * Version:     0.4-alpha
+ * Version:     0.4.1-alpha
  * Text Domain: atcf
  */
 
