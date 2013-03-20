c7ff8d885fbc88aa1e4d2ab1dccd1114940678e3
Spencer Finnell <spencerfinnell@gmail.com>
2013-03-20 09:57:49 -0400

    Redirect contributors back to homepage instead of displaying message on admin.

--- includes/roles.php
+++ includes/roles.php
@@ -16,7 +16,7 @@ function atcf_roles() {
 	global $wp_roles;
 
 	$campaign_contributor = add_role( 'campaign_contributor', 'Campaign Contributor', array(
-		'read' 						=> false,
+		'read' 						=> true,
 		'edit_posts' 				=> false,
 		'delete_posts' 				=> false
 	) );
@@ -26,5 +26,21 @@ function atcf_roles() {
 			$wp_roles = new WP_Roles();
 
 	$wp_roles->add_cap( 'campaign_contributor', 'submit_campaigns' );
+	$wp_roles->add_cap( 'campaign_contributor', 'read' );
 }
-add_action( 'admin_init', 'atcf_roles' );
+add_action( 'init', 'atcf_roles' );
+
+/**
+ * Redirect users who shouldn't be here.
+ *
+ * @since Appthemer CrowdFunding 0.7.1
+ *
+ * @return void
+ */
+function atcf_prevent_admin_access() {
+	if ( current_user_can( 'submit_campaigns' ) && ! current_user_can( 'edit_posts' ) ) {
+		wp_safe_redirect( home_url() );
+		exit;
+	}
+}
+add_action( 'admin_init', 'atcf_prevent_admin_access', 1 );
