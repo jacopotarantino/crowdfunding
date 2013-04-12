e3389a6050b13e79e1acd70393a73105d7f96c89
Spencer Finnell <spencerfinnell@gmail.com>
2013-04-12 15:34:20 -0400

    Allow themes to hook into shortcode more easily. Closes #106

--- includes/shortcode-profile.php
+++ includes/shortcode-profile.php
@@ -159,18 +159,27 @@ function atcf_shortcode_profile_campaigns( $user ) {
 	<ul class="atcf-profile-campaigns">
 	<?php if ( $campaigns->have_posts() ) : while ( $campaigns->have_posts() ) : $campaigns->the_post(); $campaign = atcf_get_campaign( get_post()->ID ); ?>
 		<li class="atcf-profile-campaign-overview">
+			<?php do_action( 'atcf_profile_campaign_before', $campaign ); ?>
+
 			<h4 class="entry-title">
 				<?php the_title(); ?>
 			</h4>
 
+			<?php do_action( 'atcf_profile_campaign_after_title', $campaign ); ?>
+
 			<?php if ( 'pending' == get_post()->post_status ) : ?>
+				<?php do_action( 'atcf_profile_campaign_pending_before', $campaign ); ?>
 				<span class="campaign-awaiting-review"><?php _e( 'This campaign is awaiting review.', 'atcf' ); ?></span>
+				<?php do_action( 'atcf_profile_campaign_pending_after', $campaign ); ?>
 			<?php else : ?>	
+				<?php do_action( 'atcf_profile_campaign_published_before', $campaign ); ?>
+
 				<ul class="actions">
 					<li><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'fundify' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php _e( 'View', 'atcf' ); ?></a></li>
 					<li><a href="<?php the_permalink(); ?>edit/" title="<?php echo esc_attr( sprintf( __( 'Edit %s', 'fundify' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php _e( 'Edit', 'atcf' ); ?></a></li>
 					<li><a href="<?php the_permalink(); ?>#comments" title="<?php echo esc_attr( sprintf( __( 'Comments for %s', 'fundify' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php _e( 'Comments', 'atcf' ); ?></a></li>
 					<li><a href="<?php the_permalink(); ?>backers/" title="<?php echo esc_attr( sprintf( __( 'Backers for %s', 'fundify' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php _e( 'Backers', 'atcf' ); ?></a></li>
+					<?php do_action( 'atcf_profile_campaign_actions_all', $campaign ); ?>
 				</ul>
 
 				<ul class="actions">
@@ -181,8 +190,12 @@ function atcf_shortcode_profile_campaigns( $user ) {
 					<?php if ( ( 'flexible' == $campaign->type() || $campaign->is_funded() ) ) : ?>
 					<li><a href="<?php echo esc_url( wp_nonce_url( add_query_arg( array( 'action' => 'atcf-request-data', 'campaign' => $campaign->ID ) ), 'atcf-request-data' ) ); ?>" title="<?php echo esc_attr( sprintf( __( 'Export data for %s', 'fundify' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php _e( 'Export Data', 'atcf' ); ?></a></li>
 					<?php endif; ?>
+					<?php do_action( 'atcf_profile_campaign_actions_special', $campaign ); ?>
 				</ul>
+
+				<?php do_action( 'atcf_profile_campaign_published_after', $campaign ); ?>
 			<?php endif; ?>
+			<?php do_action( 'atcf_profile_campaign_after', $campaign ); ?>
 		</li>	
 	<?php endwhile; endif; wp_reset_query(); ?>
 	</ul>
@@ -222,10 +235,11 @@ function atcf_shortcode_profile_info_process() {
 		wp_die( $errors );
 
 	wp_update_user( apply_filters( 'atcf_shortcode_profile_info_process_update', array(
-		'ID'            => $user->ID,
-		'description'   => $bio,
-		'user_nicename' => $nicename,
-		'user_url'      => $url
+		'ID'               => $user->ID,
+		'description'      => $bio,
+		'display_name'     => $nicename,
+		'user_nicename'    => $user->user_nicename,
+		'user_url'         => $url
 	) ) );
 
 	foreach ( _wp_get_user_contactmethods() as $method => $name ) {
