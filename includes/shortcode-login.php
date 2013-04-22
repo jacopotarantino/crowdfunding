b462473557491cf7e45c0d2b54dc3fe1b783adef
Spencer Finnell <spencerfinnell@gmail.com>
2013-04-22 10:59:52 -0400

    Only show register link if it exists.

--- includes/shortcode-login.php
+++ includes/shortcode-login.php
@@ -63,9 +63,13 @@ function atcf_shortcode_login_form_bottom() {
 	global $edd_options;
 
 	$add = '<p>
-		<a href="' . esc_url( add_query_arg( 'action', 'lostpassword', site_url( 'wp-login.php' ) ) ) . '">' . __( 'Forgot Password', 'fundify' ) . '</a> ' . _x( 'or', 'login form action divider', 'fundify' ) . '
-		 <a href="' . esc_url( get_permalink( $edd_options[ 'register_page' ] ) ) . '">' . __( 'Register', 'fundify' ) . '</a>
-	</p>';
+		<a href="' . esc_url( add_query_arg( 'action', 'lostpassword', site_url( 'wp-login.php' ) ) ) . '">' . __( 'Forgot Password', 'fundify' ) . '</a> ' . _x( 'or', 'login form action divider', 'fundify' );
+
+	if ( isset( $edd_options[ 'register_page' ] ) ) {
+		$add .= '<a href="' . esc_url( get_permalink( $edd_options[ 'register_page' ] ) ) . '">' . __( 'Register', 'fundify' ) . '</a>';
+	}
+
+	$add .= '</p>';
 
 	return $add;
 }
