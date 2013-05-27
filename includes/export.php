0acdc8425e9776e7247bb5ecc4dbc027152f647d
Spencer Finnell <spencerfinnell@gmail.com>
2013-05-27 11:47:51 -0400

    Export capability. Closes #171

--- includes/export.php
+++ includes/export.php
@@ -11,6 +11,18 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 /**
+ * Export capability shim for Easy Digital Downloads
+ *
+ * @since Appthemer CrowdFunding 1.3
+ *
+ * @return boolean If a user can export campaign data.
+ */
+function atcf_export_capability() {
+	return current_user_can( 'submit_campaigns' );
+}
+add_filter( 'edd_export_capability', 'atcf_export_capability' );
+
+/**
  * Export action. Monitor backened and create a new export.
  *
  * @since Appthemer CrowdFunding 0.1-alpha
