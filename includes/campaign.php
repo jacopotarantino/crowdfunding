c5b90121ec395682c44eb2c118a0ed601d3b5b34
Spencer Finnell <spencerfinnell@gmail.com>
2013-07-10 09:04:41 -0400

    Avoid error when logging payment notes.

--- includes/campaign.php
+++ includes/campaign.php
@@ -352,7 +352,7 @@ class ATCF_Campaigns {
 
 				/** Loop through each gateway's failed payments */
 				foreach ( $gateway as $payment_id ) {
-					edd_insert_payment_note( $payment_id, apply_filters( 'atcf_failed_payment_note', sprintf( __( 'Error processing preapproved payment via %s', 'atcf' ), edd_get_gateway_admin_label( $_gateway ) ) ) );
+					edd_insert_payment_note( $payment_id, apply_filters( 'atcf_failed_payment_note', sprintf( __( 'Error processing preapproved payment via %s', 'atcf' ), $_gateway ) ) );
 
 					$failed_count++;
 
